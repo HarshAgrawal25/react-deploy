a198afaaa1d0bd9b98d879464e45ae98b0806867
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-11-06 11:39:34 +0530

    Updates

--- precache-manifest.4713374ffc48bd664a0a3ff214150f61.js
+++ precache-manifest.4713374ffc48bd664a0a3ff214150f61.js
@@ -1,47 +1,47 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a87db9f985054042a1547febdb0d21bb",
+    "revision": "0047cd3a891c5c9eb7d2d2066afbd46b",
     "url": "/react-deploy/index.html"
   },
   {
-    "revision": "80c8dcefe167ce62cfb7",
+    "revision": "8203565cd5ba4012d623",
     "url": "/react-deploy/static/css/2.c3594674.chunk.css"
   },
   {
-    "revision": "af53f5735f7488690311",
+    "revision": "62d8fe09c8b716426c87",
     "url": "/react-deploy/static/css/3.0ad35584.chunk.css"
   },
   {
-    "revision": "d5cbfe33572e898ac47d",
-    "url": "/react-deploy/static/css/main.e28502bc.chunk.css"
+    "revision": "ad71d9380e882da64241",
+    "url": "/react-deploy/static/css/main.05818b31.chunk.css"
   },
   {
-    "revision": "80c8dcefe167ce62cfb7",
-    "url": "/react-deploy/static/js/2.31296b14.chunk.js"
+    "revision": "8203565cd5ba4012d623",
+    "url": "/react-deploy/static/js/2.b221cafe.chunk.js"
   },
   {
     "revision": "da4ed30ef64bd9e65b109b27cfd63f4f",
-    "url": "/react-deploy/static/js/2.31296b14.chunk.js.LICENSE.txt"
+    "url": "/react-deploy/static/js/2.b221cafe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "af53f5735f7488690311",
-    "url": "/react-deploy/static/js/3.8b642772.chunk.js"
+    "revision": "62d8fe09c8b716426c87",
+    "url": "/react-deploy/static/js/3.3cc04911.chunk.js"
   },
   {
     "revision": "d81c6c91afd2bb4bf8caccddf0472235",
-    "url": "/react-deploy/static/js/3.8b642772.chunk.js.LICENSE.txt"
+    "url": "/react-deploy/static/js/3.3cc04911.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c69c8d977a3a7e61fd50",
-    "url": "/react-deploy/static/js/4.b175684b.chunk.js"
+    "revision": "8d8d3ea1b49c87074672",
+    "url": "/react-deploy/static/js/4.f155e910.chunk.js"
   },
   {
-    "revision": "d5cbfe33572e898ac47d",
-    "url": "/react-deploy/static/js/main.d40ccf29.chunk.js"
+    "revision": "ad71d9380e882da64241",
+    "url": "/react-deploy/static/js/main.9d3f41a1.chunk.js"
   },
   {
-    "revision": "a4b83288e518e7352647",
-    "url": "/react-deploy/static/js/runtime-main.c4810519.js"
+    "revision": "035b679e2fb2e666d0c7",
+    "url": "/react-deploy/static/js/runtime-main.d600a6e3.js"
   },
   {
     "revision": "8107f8b787a40a84fbf3ee220abd84aa",
