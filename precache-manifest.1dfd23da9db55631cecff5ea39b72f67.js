a40e08a65b6a7758ff09a01303a4c5e267cc4a8a
Harsh Agrawal <agrawalharsh199925@gmail.com>
2020-12-04 13:34:06 +0530

    Updates

---
+++ precache-manifest.1dfd23da9db55631cecff5ea39b72f67.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d0e7a0550c73c211de4f99b669c72e",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1c1a3d2217af5990c334",
+    "url": "/react-deploy/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "62d8fe09c8b716426c87",
+    "url": "/react-deploy/static/css/3.0ad35584.chunk.css"
+  },
+  {
+    "revision": "b20e43d9c05ac190aa9d",
+    "url": "/react-deploy/static/css/main.f94aa22a.chunk.css"
+  },
+  {
+    "revision": "1c1a3d2217af5990c334",
+    "url": "/react-deploy/static/js/2.33a65780.chunk.js"
+  },
+  {
+    "revision": "da4ed30ef64bd9e65b109b27cfd63f4f",
+    "url": "/react-deploy/static/js/2.33a65780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d8fe09c8b716426c87",
+    "url": "/react-deploy/static/js/3.3cc04911.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/react-deploy/static/js/3.3cc04911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b50af1721d387e4083b2",
+    "url": "/react-deploy/static/js/4.1dba802a.chunk.js"
+  },
+  {
+    "revision": "b20e43d9c05ac190aa9d",
+    "url": "/react-deploy/static/js/main.9d3f41a1.chunk.js"
+  },
+  {
+    "revision": "617f7286ff3212e3029b",
+    "url": "/react-deploy/static/js/runtime-main.4026d3bd.js"
+  },
+  {
+    "revision": "9eb151ef4e50c8ee9c25bba859c992a6",
+    "url": "/react-deploy/static/media/SAMARN__.9eb151ef.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-deploy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-deploy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-deploy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-deploy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-deploy/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
